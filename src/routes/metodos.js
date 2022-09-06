fa1b2a1a14454a218c42020785ff0db56dfd49df
Juan Torres <juantorresvelilla@gmail.com>
2022-09-06 09:13:32 -0500

    Actualización de control de codigo en actualizar

--- src/routes/metodos.js
+++ src/routes/metodos.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const router = Router();
+const _ = require('underscore');
 
 const productos = require('../Productos.json');
 
@@ -33,58 +34,6 @@ router.post('/', (req, res) => {
     }
 });
 
-router.get('/', (req, res) => {
-    var precioFinal = 0;
-    precioFinal = productos.precio - (productos.precio * productos.descuento) + (productos.precio + productos.iva) 
-    res.json(productos);
-});
-
-//consultar productos
-router.get('/:Sku', (req, res) => {
-    const {Sku} = req.params;
-    _.each(productos, (producto, i) => {
-        if(producto.Sku == Sku){
-            res.json(producto);
-        }
-    });
-    res.json(producto);
-});
-
-//insertar producto
-router.post('/', (req, res) => {
-    const { Nombre, Precio,URL, Marca, Iva, Inventario}  = req.body;
-    if(Nombre && Precio && URL && Marca && Iva && Inventario){
-        const Sku = productos.length + 1;
-        const newProduct = {...req.body, Sku};
-        productos.push(newProduct);
-        res.send('se insertó el producto satisfactoriamente.');
-    }else{
-        res.send('No se inserto el Producto satisfactoriamente');
-    }
-});
-
-//actualizar producto
-router.put('/:Sku', (req, res) => {
-    const {Sku} = req.params;
-    const { Nombre, Precio, URL, Marca, Iva, Inventario}  = req.body;
-    if(Nombre && Precio && URL && Marca && Iva && Inventario){
-        _.each(productos, (producto, i) => {
-            if(producto.Sku == Sku){
-                producto.Nombre = Nombre;
-                producto.Precio = Precio;
-                producto.URL = URL;
-                producto.Marca = Marca;
-                producto.Iva = Iva;
-                producto.Inventario = Inventario;                
-            }
-           });
-           res.send('Actualizado');
-           //res.json(productos);
-    }else{
-        res.status(500).json({error: 'No actualizado'});
-    }
-
-});
 
 //actualizar producto
 router.put('/:Sku', (req, res) => {
