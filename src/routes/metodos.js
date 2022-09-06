61b2f5954b2257f3c68a74e058a5e0465b54584f
Juan Torres <juantorresvelilla@gmail.com>
2022-09-06 02:53:07 -0500

    rama insertar producto realizado

--- src/routes/metodos.js
+++ src/routes/metodos.js
@@ -20,4 +20,23 @@ router.get('/:Sku', (req, res) => {
     res.json(producto);
 });
 
+//insertar producto
+router.post('/', (req, res) => {
+    const { Nombre, Precio,URL, Marca, Iva, Inventario}  = req.body;
+    if(Nombre && Precio && URL && Marca && Iva && Inventario){
+        const Sku = productos.length + 1;
+        const newProduct = {...req.body, Sku};
+        productos.push(newProduct);
+        res.send('se insertó el producto satisfactoriamente.');
+    }else{
+        res.send('No se inserto el Producto satisfactoriamente');
+    }
+});
+
+router.get('/', (req, res) => {
+    var precioFinal = 0;
+    precioFinal = productos.precio - (productos.precio * productos.descuento) + (productos.precio + productos.iva) 
+    res.json(productos);
+});
+
 module.exports = router;
