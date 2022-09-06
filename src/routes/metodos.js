beec0c8425d758f67cd7f03fcfd79ad60ee455e9
Juan Torres <juantorresvelilla@gmail.com>
2022-09-05 22:13:00 -0500

    rama consultar producto realizado

--- src/routes/metodos.js
+++ src/routes/metodos.js
@@ -9,5 +9,15 @@ router.get('/', (req, res) => {
     res.json(productos);
 });
 
+//consultar productos
+router.get('/:Sku', (req, res) => {
+    const {Sku} = req.params;
+    _.each(productos, (producto, i) => {
+        if(producto.Sku == Sku){
+            res.json(producto);
+        }
+    });
+    res.json(producto);
+});
 
 module.exports = router;
